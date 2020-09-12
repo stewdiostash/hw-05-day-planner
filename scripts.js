57ec632a01e6cee2a4cd0dfacf902bac78d4d10e
Andrew Stewart <stewdio@Andrews-Air.attlocal.net>
2020-09-12 18:08:36 -0400

    commented js file

--- scripts.js
+++ scripts.js
@@ -1,83 +1,83 @@
-var workDay = JSON.parse(localStorage.getItem("workDay"));
+$(document).ready(function () {
+  
+  // Add today's date to header
 
-if (!workDay) {
-  workDay = [
-    {
-      time: "9:00 AM",
-      note: "",
-    },
-    {
-      time: "10:00 AM",
-      note: "",
-    },
-    {
-      time: "11:00 AM",
-      note: "",
-    },
-    {
-      time: "12:00 PM",
-      note: "",
-    },
-    {
-      time: "1:00 PM",
-      note: "",
-    },
-    {
-      time: "2:00 PM",
-      note: "",
-    },
-    {
-      time: "3:00 PM",
-      note: "",
-    },
-    {
-      time: "4:00 PM",
-      note: "",
-    },
-    {
-      time: "5:00 PM",
-      note: "",
-    },
-  ];
-}
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-for (var i = 0; i < workDay.length; i++) {
-  var newRow = $("<div class='row'>");
-    newRow.attr("data-index", i);
-  var timeArea = $("<div class='col-2 col-md-1 hour'>").text(workDay[i].time);
-  var textArea = $("<textarea class='col-8 col-md-10 notepad'>");
-    textArea.val(workDay[i].note);
-  var saveArea = $("<div class='col-2 col-md-1 saveBtn'>");
-  var saveIcon = $("<i class='fas fa-save'>");
+  // Set workDay array
 
-  if (moment().format("H") > i + 9) {
-    textArea.addClass("past");
-  } else if (moment().format("H") == i + 9) {
-    textArea.addClass("present");
-  } else {
-    textArea.addClass("future");
+  var workDay = JSON.parse(localStorage.getItem("workDay"));
+
+  if (!workDay) {
+    workDay = [
+      {
+        time: "9:00 AM",
+        note: "",
+      },
+      {
+        time: "10:00 AM",
+        note: "",
+      },
+      {
+        time: "11:00 AM",
+        note: "",
+      },
+      {
+        time: "12:00 PM",
+        note: "",
+      },
+      {
+        time: "1:00 PM",
+        note: "",
+      },
+      {
+        time: "2:00 PM",
+        note: "",
+      },
+      {
+        time: "3:00 PM",
+        note: "",
+      },
+      {
+        time: "4:00 PM",
+        note: "",
+      },
+      {
+        time: "5:00 PM",
+        note: "",
+      },
+    ];
   }
 
-  $("#calendar").append(newRow);
-  newRow.append(timeArea, textArea, saveArea);
-  saveArea.append(saveIcon);
-}
+  // Create and populate each row
 
-// $(".fas").on("click", function () {
-//   var noteNumber = $(this).parent().parent().attr("data-index");
-//   workDay[noteNumber].note = $(this).parent().siblings(".notepad").val();
-//   localStorage.setItem("workDay", JSON.stringify(workDay));
-// });
+  for (var i = 0; i < workDay.length; i++) {
+    var newRow = $("<div class='row'>");
+    newRow.attr("data-index", i);
+    var timeArea = $("<div class='col-2 col-md-1 hour'>").text(workDay[i].time);
+    var textArea = $("<textarea class='col-8 col-md-10 notepad'>");
+    textArea.val(workDay[i].note);
+    var saveArea = $("<div class='col-2 col-md-1 saveBtn'>");
+    var saveIcon = $("<i class='fas fa-save'>");
 
-var today = moment().format('dddd, MMMM Do YYYY')
+    if (moment().format("H") > i + 9) {
+      textArea.addClass("past");
+    } else if (moment().format("H") == i + 9) {
+      textArea.addClass("present");
+    } else {
+      textArea.addClass("future");
+    }
 
-$("#currentDay").text(today);
+    $("#calendar").append(newRow);
+    newRow.append(timeArea, textArea, saveArea);
+    saveArea.append(saveIcon);
+  }
 
+  // Index and store notes to localStorage on save click
 
-
-$(".saveBtn").on("click", function () {
+  $(".saveBtn").on("click", function () {
     var noteNumber = $(this).parent().attr("data-index");
     workDay[noteNumber].note = $(this).siblings(".notepad").val();
     localStorage.setItem("workDay", JSON.stringify(workDay));
   });
-  
+});
